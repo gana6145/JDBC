275976aee837638724b811ac4fb6caeafe71884b
Ganesh Jadhav <jadhav.ganesh1700@gmail.com>
2025-11-17 10:00:05 +0530

    created the product table in padmin and created the method to insert data update fetch all data and fetch by id and fetch by price and update price to deep dive int the jdbc also created the stored procedure to insert data into the db pgAdmin and successfully created the method for to insert the recod using the Callable Statemet prepare call method

---
+++ src/main/java/com/product/ProductCRUDOperation.java
@@ -0,0 +1,141 @@
+package com.product;
+
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Scanner;
+
+public class ProductCRUDOperation {
+	public static String url="jdbc:postgresql://localhost:5432/qsp?user=postgres&password=root";
+	public static Connection con;
+	static Scanner scan=new Scanner(System.in);
+
+	
+	static {
+		try {
+			Class.forName("org.postgresql.Driver");
+			con=DriverManager.getConnection(url);
+		} catch (ClassNotFoundException | SQLException e) {
+			
+			e.printStackTrace();
+		}
+	}
+	
+	  public  static void insertProduct() throws SQLException {
+			PreparedStatement cs= con.prepareStatement("insert into product values(?,?,?)");
+			System.out.println("Enter the product ID");
+			cs.setInt(1, scan.nextInt());
+			System.out.println("Enter the product name:");
+			cs.setString(2, scan.next());
+			System.out.println("Entre the product price:");
+			cs.setDouble(3, scan.nextDouble());
+			cs.execute();
+			System.out.println("Product inserted Successfully.");
+			
+		}
+	  
+	  public  static void insertProducts() throws SQLException {
+			CallableStatement cs= con.prepareCall("call insert_product(?,?,?)");
+			System.out.println("Enter the product ID");
+			cs.setInt(1, scan.nextInt());
+			System.out.println("Enter the product name:");
+			cs.setString(2, scan.next());
+			System.out.println("Entre the product price:");
+			cs.setDouble(3, scan.nextDouble());
+			cs.execute();
+			System.out.println("Product inserted Successfully.");
+			
+		}
+	  
+	  
+	  public  static void updateProductPrice() throws SQLException {
+			PreparedStatement cs= con.prepareStatement("update  product set price=? where id=?");
+			System.out.println("Enter the product ID");
+			System.out.println("Enter the product id");
+			cs.setInt(1, scan.nextInt());
+			System.out.println("Entre the new price for the product:");
+			cs.setDouble(2, scan.nextDouble());
+			cs.execute();
+			System.out.println("Product price updated Successfully.");
+			
+		}
+	  
+	  public  static void deleteProduct() throws SQLException {
+			PreparedStatement cs= con.prepareStatement("delete from product where id=?");
+			System.out.println("Enter the product ID which you want to delete");
+			cs.setInt(1, scan.nextInt());
+			cs.execute();
+			System.out.println("Product deleted Successfully.");
+			
+		}
+	  
+	  public  static void findById() throws SQLException {
+			PreparedStatement cs= con.prepareStatement("select * from product where id=?");
+			System.out.println("Enter the product ID which you want to delete");
+			cs.setInt(1, scan.nextInt());
+			ResultSet rs=cs.executeQuery();
+			
+			while(rs.next()) {
+				System.out.println("id:" + rs.getInt(1)+ " Name:" +rs.getString(2) + " Price:" +rs.getDouble(3));
+				return;
+			}
+			System.out.println("Product not found .");
+			
+		}
+	  public  static void findByPrice() throws SQLException {
+			PreparedStatement cs= con.prepareStatement("select * from product where price=?");
+			System.out.println("Enter the product price to find the product");
+			cs.setDouble(1, scan.nextDouble());
+			ResultSet rs=cs.executeQuery();
+			
+			
+			while(rs.next()) {
+				System.out.println("id:" + rs.getInt(1)+ " Name:" +rs.getString(2) + " Price:" +rs.getDouble(3));
+				
+			}
+//			System.out.println("Product not found .");
+			
+		}
+	  
+	  public  static void findAll() throws SQLException {
+			PreparedStatement cs= con.prepareStatement("select * from product");
+			
+			ResultSet rs=cs.executeQuery();
+			
+			while(rs.next()) {
+				System.out.println("id:" + rs.getInt(1)+ " Name:" +rs.getString(2) + " Price:" +rs.getDouble(3));
+				
+			}
+	  }
+	  
+	  public static void closeConnection() throws SQLException {
+		  con.close();
+		  System.out.println("connection closes.");
+		  
+	  }
+			
+		
+	  
+	  
+	public static void main(String[] args) {
+		
+		try {
+//			insertProduct();
+////			updateProductPrice();
+////			deleteProduct();
+////			findById();
+//			findAll();
+			insertProducts();
+//			findByPrice();
+			closeConnection();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+
+}
